86a987ca645fda5326efc9d4a4d000c51a1b5e4b
ajin3830 <ajin3830@gmail.com>
2023-01-18 09:32:43 -0500

    completed lab

--- src/index.js
+++ src/index.js
@@ -13,3 +13,45 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+function fetchToys(){
+fetch("http://localhost:3000/toys")
+ .then(res => res.json())
+ .then(json => renderToys(json))
+}
+
+function renderToys(toys) {
+  toys.forEach((toy) => {
+    const toyCollection = document.querySelector("#toy-collection")
+    const card = document.createElement("div")
+    card. className = 'card'
+
+    const h2 = document.createElement('h2');
+    h2.textContent = toy.name;
+
+    const img = document.createElement('img');
+    img.src = toy.img;
+    img.className = 'toy-avatar';
+
+    const p = document.createElement('p');
+    p.textContent = toy.likes
+
+    const button = document.createElement('button');
+    button.className = 'like-btn';
+    button.id = 'toy_id'
+    button.textContent = 'like';
+
+    card.appendChild(h2);
+
+    card.append(h2, img)
+
+  })
+}
+
+
+ 
+  
+
+
+  
+
