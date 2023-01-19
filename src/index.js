0a3870d44257dacf0167b79f2ed96981b8703cb1
ajin3830 <ajin3830@gmail.com>
2023-01-18 23:53:49 -0500

    completed lab needing a fix

--- src/index.js
+++ src/index.js
@@ -15,42 +15,92 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 function fetchToys(){
-fetch("http://localhost:3000/toys")
+fetch('http://localhost:3000/toys')
  .then(res => res.json())
- .then(json => renderToys(json))
+ .then(toyData => renderToys(toyData))
 }
+
+fetchToys()
 
 function renderToys(toys) {
-  toys.forEach((toy) => {
-    const toyCollection = document.querySelector("#toy-collection")
-    const card = document.createElement("div")
-    card. className = 'card'
 
-    const h2 = document.createElement('h2');
-    h2.textContent = toy.name;
+  toys.forEach(toy => {
+    const toyCollection = document.querySelector('#toy-collection')
+    const card = document.createElement('div')
+    card.className = 'card'
+    
 
-    const img = document.createElement('img');
-    img.src = toy.img;
-    img.className = 'toy-avatar';
-
-    const p = document.createElement('p');
+    const h2 = document.createElement('h2')
+    h2.textContent = toy.name
+    
+    const img = document.createElement('img')
+    img.src = toy.image
+    img.className = 'toy-avatar'
+  
+    const p = document.createElement('p')
     p.textContent = toy.likes
+  
+    const button = document.createElement('button')
+    button.className = 'like-btn'
+    button.id = toy.id
+    button.textContent = 'Like'
+    // ^^^giving our buttons a name so the name appears on the button
 
-    const button = document.createElement('button');
-    button.className = 'like-btn';
-    button.id = 'toy_id'
-    button.textContent = 'like';
+    // below is Patch
+    button.addEventListener('click', () =>{
+      
+      p.textContent = parseInt(p.textContent) + 1
 
-    card.appendChild(h2);
+      fetch(`http://localhost:3000/toys/${toy.id}`, {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+           Accept: "application/json"},
+        body: JSON.stringify({
+          likes: parseInt(p.textContent)
+        })
+      })
+      .then(r => r.json())
+      .then(toyObj => renderToys([toyObj]))
 
-    card.append(h2, img)
+    })
 
+    card.append(h2, img, p, button)
+    toyCollection.appendChild(card)
   })
+
 }
 
+function addNewToy() {
+  const form = document.querySelector("body > div.container > form")
+  form.addEventListener('submit', (e)=> {
+    e.preventDefault()
+    const inputName = e.target['name'].value
+    const inputImage = e.target['image'].value
+    
+    const newToy = {
+      name: inputName,
+      image: inputImage,
+      likes: 0
 
- 
-  
+    }
+    // renderToys([newToy]) 
+    // ^^^commented out to make post request w pessimistic rendering
+    fetch('http://localhost:3000/toys', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+         Accept: "application/json"},
+      body: JSON.stringify(newToy)
+    })
+    .then(r => r.json())
+    .then(toyObj => renderToys([toyObj]))
+    // ^^^if commented out both then and uncommented out renderToys([newToy])
+    // it would be optimistic rendering
+    })
+}
+
+addNewToy()
 
 
   
